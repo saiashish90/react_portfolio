f9c0d6c97585ffe534f33b0cdfd5cf1fd38ced5b
saiashish90 <saiashish90@gmail.com>
2020-07-26 19:00:01 +0530

    Changed the scroll function

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
 	//Set the height of the body to the height of the scrolling div
 	const setBodyHeight = () => {
-		document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
+		document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height + 200}px`;
 	};
 
 	// Scrolling
