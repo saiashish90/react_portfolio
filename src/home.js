cfb9b2591fdd6b79a0128bdecd3448badc2b251f
saiashish90 <saiashish90@gmail.com>
2020-07-26 13:59:36 +0530

    stack changes and mobile view navigation

--- src/home.js
+++ src/home.js
@@ -1,39 +1,104 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './css/styles.css';
+
+import Particles from 'react-tsparticles';
 
 function home() {
 	return (
-		<section className="h-screen w-100 flex flex-row items-center" id="home">
-			<div className="w-screen flex flex-wrap overflow-hidden ">
-				<div className="w-full overflow-hidden md:w-1/2 text-white flex items-center">
-					<div className="mx-auto">
-						<h1 className="m-auto text-6xl font-bold">
-							Hi,<br />I am <span className="text-pallete-highlight">Ashish</span>
-						</h1>
-						<button className="bg-pallete-highlight text-black font-semibold text-sm py-3 px-8 rounded-lg">
-							Contact
-						</button>
+		<Fragment>
+			<Particles
+				className="absolute h-screen w-screen"
+				style={{ zIndex: -1 }}
+				id="tsparticles"
+				options={{
+					particles     : {
+						number      : {
+							value   : 30,
+							density : {
+								enable     : true,
+								value_area : 700
+							}
+						},
+						color       : {
+							value : '#45a29e'
+						},
+						shape       : {
+							type    : 'circle',
+							stroke  : {
+								width : 0,
+								color : '#000000'
+							},
+							polygon : {
+								nb_sides : 5
+							}
+						},
+						opacity     : {
+							value  : 1,
+							random : true,
+							anim   : {
+								enable      : false,
+								speed       : 1,
+								opacity_min : 0.1,
+								sync        : false
+							}
+						},
+						size        : {
+							value  : 3,
+							random : true,
+							anim   : {
+								enable   : false,
+								speed    : 40,
+								size_min : 0.1,
+								sync     : false
+							}
+						},
+						line_linked : {
+							enable   : true,
+							distance : 150,
+							color    : '#ffffff',
+							opacity  : 0.1,
+							width    : 1
+						},
+						move        : {
+							enable    : true,
+							speed     : 1,
+							direction : 'none',
+							random    : true,
+							straight  : false,
+							out_mode  : 'out',
+							bounce    : false,
+							attract   : {
+								enable  : false,
+								rotateX : 600,
+								rotateY : 1200
+							}
+						}
+					},
+					retina_detect : true
+				}}
+			/>
+			<section className="h-screen w-100 flex flex-row items-center" id="home">
+				<div className="w-screen flex flex-wrap overflow-hidden ">
+					<div className="w-full overflow-hidden md:w-1/2 text-white flex items-center">
+						<div className="mx-auto">
+							<h1 className="m-auto text-6xl font-bold">
+								Hi,<br />I am <span className="text-pallete-highlight">Ashish</span>
+							</h1>
+							<button className="bg-pallete-highlight text-black font-semibold text-sm py-3 px-8 rounded-lg">
+								Contact
+							</button>
+						</div>
+					</div>
+					<div className="w-full hidden md:block overflow-hidden md:w-1/2">
+						<img
+							className="w-8/12 mx-auto relative z-10"
+							src={require('./assets/img/profile.png')}
+							alt="img"
+						/>
 					</div>
 				</div>
-				<div className="w-full hidden md:block overflow-hidden md:w-1/2">
-					<img className="w-8/12 mx-auto" src={require('./assets/img/profile.png')} alt="img" />
-				</div>
-			</div>
-			{/* <div className="w-1/2 flex ">
-				<div className="mx-auto">
-					<h1 className="text-white text-5xl font-semibold">
-						Hi,<br />I am <span className="text-pallete-highlight">Ashish</span>
-					</h1>
-					<button className="bg-pallete-highlight text-black font-semibold text-sm py-3 px-8 rounded-lg">
-						Contact
-					</button>
-				</div>
-			</div>
-
-			<div className="w-1/2 invisible lg:visible">
-				<img className=" mx-auto w-2/4" src={require('./assets/img/profile.png')} alt="" />
-			</div> */}
-		</section>
+			</section>
+		</Fragment>
 	);
 }
 
