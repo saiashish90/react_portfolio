cc2c04df6d9bd7737fb4fbc8cf532694ed960544
saiashish90 <saiashish90@gmail.com>
2020-07-26 18:36:14 +0530

    Smooth scroll one fully

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useRef, useEffect } from 'react';
+
+import Header from './header/header';
+import Home from './home';
+import About from './about';
+import useWindowSize from './hooks/useWindowSize';
+
+import './css/styles.css';
+import './css/App.css';
+
+function App() {
+	const size = useWindowSize();
+	const app = useRef();
+	const scrollContainer = useRef();
+
+	// Configs
+	const data = {
+		ease     : 0.1,
+		current  : 0,
+		previous : 0,
+		rounded  : 0
+	};
+
+	// Run scrollrender once page is loaded.
+	useEffect(() => {
+		requestAnimationFrame(() => skewScrolling());
+	}, []);
+	//set the height of the body.
+	useEffect(
+		() => {
+			setBodyHeight();
+		},
+		[ size.height ]
+	);
+
+	//Set the height of the body to the height of the scrolling div
+	const setBodyHeight = () => {
+		document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
+	};
+
+	// Scrolling
+	const skewScrolling = () => {
+		//Set Current to the scroll position amount
+		data.current = window.scrollY;
+		// Set Previous to the scroll previous position
+		data.previous += (data.current - data.previous) * data.ease;
+		// Set rounded to
+		data.rounded = Math.round(data.previous * 100) / 100;
+
+		// Difference between
+		const difference = data.current - data.rounded;
+		const acceleration = difference / size.width;
+		const velocity = +acceleration;
+		const skew = velocity * 7.5;
+
+		//Assign skew and smooth scrolling to the scroll container
+		scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`;
+
+		//loop vai raf
+		requestAnimationFrame(() => skewScrolling());
+	};
+
+	return (
+		<div ref={app} className="App">
+			<Header />
+			<div ref={scrollContainer} className="scroll">
+				<Home />
+				<About />
+				<p />
+			</div>
+		</div>
+	);
+}
+
+export default App;
