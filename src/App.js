02e31c98ba481cc87243203edfd0400a1131a54d
saiashish90 <saiashish90@gmail.com>
2020-07-27 20:32:10 +0530

    fixed issue with icons

--- src/App.js
+++ src/App.js
@@ -31,19 +31,12 @@ function App() {
 			// Set rounded to
 			data.rounded = Math.round(data.previous * 100) / 100;
 
-			// Difference between
-			const difference = data.current - data.rounded;
-			const acceleration = difference / size.width;
-			const velocity = +acceleration;
-			const skew = velocity * 7.5;
-
-			//Assign skew and smooth scrolling to the scroll container
-			scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`;
+			scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0)`;
 
 			//loop vai raf
 			requestAnimationFrame(() => skewScrolling());
 		},
-		[ data, size.width ]
+		[ data ]
 	);
 
 	// Run scrollrender once page is loaded.
