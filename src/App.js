f4c3b3b9ca3eee8961eb2eae4583603a75ebd3c2
saiashish90 <saiashish90@gmail.com>
2020-07-27 21:07:02 +0530

    last try

--- src/App.js
+++ src/App.js
@@ -10,39 +10,26 @@ import './css/styles.css';
 import './css/App.css';
 
 function App() {
+	// Hook to grab window size
 	const size = useWindowSize();
+
+	// Ref for parent div and scrolling div
 	const app = useRef();
 	const scrollContainer = useRef();
 
 	// Configs
-	const data = {
+	const scrollData = {
 		ease     : 0.08,
 		current  : 0,
 		previous : 0,
 		rounded  : 0
 	};
-	// Scrolling
-	const skewScrolling = () => {
-		//Set Current to the scroll position amount
-		data.current = window.scrollY;
-		// Set Previous to the scroll previous position
-		data.previous += (data.current - data.previous) * data.ease;
-		// Set rounded to
-		data.rounded = Math.round(data.previous * 100) / 100;
-
-		scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0)`;
-
-		//loop vai raf
-		requestAnimationFrame(() => skewScrolling());
-	};
 
 	// Run scrollrender once page is loaded.
-	useEffect(
-		() => {
-			requestAnimationFrame(() => skewScrolling());
-		},
-		[ skewScrolling ]
-	);
+	useEffect(() => {
+		requestAnimationFrame(() => skewScrolling());
+	}, []);
+
 	//set the height of the body.
 	useEffect(
 		() => {
@@ -53,7 +40,21 @@ function App() {
 
 	//Set the height of the body to the height of the scrolling div
 	const setBodyHeight = () => {
-		document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height + 150}px`;
+		document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
+	};
+
+	// Scrolling
+	const skewScrolling = () => {
+		//Set Current to the scroll position amount
+		scrollData.current = window.scrollY;
+		// Set Previous to the scroll previous position
+		scrollData.previous += (scrollData.current - scrollData.previous) * scrollData.ease;
+		// Set rounded to
+		scrollData.rounded = Math.round(scrollData.previous * 100) / 100;
+		scrollContainer.current.style.transform = `translate3d(0, -${scrollData.rounded}px, 0)`;
+
+		//loop vai raf
+		requestAnimationFrame(() => skewScrolling());
 	};
 	return (
 		<div ref={app} className="App">
