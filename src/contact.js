bdae3314c35da6364d6cad2a1b29a65ee927dada
saiashish90 <saiashish90@gmail.com>
2020-08-01 20:11:47 +0530

    Improved contact form

--- src/contact.js
+++ src/contact.js
@@ -20,14 +20,14 @@ function contact() {
 			<h1 className="text-primaryHighlight text-4xl mb-10">Contact</h1>
 
 			<div className="flex flex-wrap">
-				<div className="text-white text-xl text-center md:text-left px-10 w-full md:w-1/2 mb-4 self-center">
+				<div className="text-white text-xl text-center md:text-left px-0 md:pl-20 md:pr-0 w-full md:w-1/2 mb-10 self-center">
 					<h1>
 						If you would like me to help you with something,<br /> email me at :
 						<span className="text-secondaryHighlight">&nbsp;saiashish90@gmail.com</span>
 					</h1>
 					<h1>Or just leave a message.</h1>
 					<h1 className="my-4">You can also find me at:</h1>
-					<div className="text-white">
+					<div className="text-white ">
 						<a
 							href="https://www.linkedin.com/in/sai-ashish-ba927a1b2/"
 							target="_blank"
@@ -50,13 +50,13 @@ function contact() {
 							rel="noopener noreferrer"
 							aria-label="coderank"
 						>
-							<i className="bx text-5xl mr-10 bx-code-alt hover:text-primaryHighlight" />
+							<i className="bx text-5xl bx-code-alt hover:text-primaryHighlight" />
 						</a>
 					</div>
 				</div>
 				<div className="w-full md:w-1/2 mb-5">
-					<form onSubmit={handleForm} className="w-3/4 mx-auto" autoComplete="off">
-						<h1 className="text-primaryHighlight text-2xl text-left mb-8">Leave a message</h1>
+					<form onSubmit={handleForm} className="w-11/12 md:w-3/4 mx-auto" autoComplete="off">
+						<h1 className="text-primaryHighlight text-2xl mb-8">Leave a message</h1>
 						<div className="mx-auto h-12 p-2 border w-full mb-8 rounded-lg text-left">
 							<input
 								className="block w-full outline-none text-white bg-transparent"
