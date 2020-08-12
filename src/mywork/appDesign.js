f1f74e906693bc47321eb6ed5af96813fb130dfc
saiashish90 <saiashish90@gmail.com>
2020-08-12 11:50:31 +0530

    App page

--- src/mywork/appDesign.js
+++ src/mywork/appDesign.js
@@ -18,18 +18,86 @@ function AppDesign() {
 						{(src) => <img className="my-5 mx-auto w-10/12" src={src} alt="project" />}
 					</ProgressiveImage>
 				</div>
+				{/* section 2 */}
+				<div
+					className="my-5 py-8 px-5 md:px-12 w-11/12 md:w-3/4 mx-auto bg-secondaryDark rounder smoothAfShadow"
+					id="work"
+				>
+					<div className="flex flex-wrap overflow-hidden text-white">
+						<div className="w-full p-5 overflow-hidden text-center md:w-1/2 lg:w-1/3 mb-5">
+							{/* work5 */}
+							<div className="shadow m-7 mb-5 rounded-lg overflow-hidden">
+								<ProgressiveImage
+									src={require('../assets/img/appDesign/Signup.png')}
+									placeholder={require('../assets/img/appDesign/Signup_tn.jpg')}
+								>
+									{(src) => <img className="w-full" src={src} alt="work3" />}
+								</ProgressiveImage>
+							</div>
+							{/* work5 */}
+						</div>
+						<div className="w-full p-5 overflow-hidden text-center md:w-1/2 lg:w-1/3 mb-5">
+							{/* work3 */}
+							<div className="shadow m-7 mb-5 rounded-lg overflow-hidden">
+								<ProgressiveImage
+									src={require('../assets/img/appDesign/Login.png')}
+									placeholder={require('../assets/img/appDesign/Login_tn.jpg')}
+								>
+									{(src) => <img className="w-full" src={src} alt="work3" />}
+								</ProgressiveImage>
+							</div>
+							{/* work3 */}
+						</div>
+						<div className="w-full p-5 overflow-hidden text-center md:w-1/2 lg:w-1/3 mb-5">
+							{/* work4 */}
+							<div className="shadow m-7 mb-5 rounded-lg overflow-hidden">
+								<ProgressiveImage
+									src={require('../assets/img/appDesign/otp.png')}
+									placeholder={require('../assets/img/appDesign/otp_tn.jpg')}
+								>
+									{(src) => <img className="w-full" src={src} alt="work3" />}
+								</ProgressiveImage>
+							</div>
+							{/* work4 */}
+						</div>
+						<div className="w-full p-5 overflow-hidden text-center md:w-1/2 lg:w-1/3 mb-5">
+							{/* work2 */}
+							<div className="shadow m-7 mb-5 rounded-lg overflow-hidden">
+								<ProgressiveImage
+									src={require('../assets/img/appDesign/grocery.png')}
+									placeholder={require('../assets/img/appDesign/grocery_tn.jpg')}
+								>
+									{(src) => <img className="w-full" src={src} alt="work2" />}
+								</ProgressiveImage>
+							</div>
+							{/* work2 */}
+						</div>
+						<div className="w-full p-5 overflow-hidden text-center md:w-1/2 lg:w-1/3 mb-5">
+							{/* work1 */}
+							<div className="shadow m-7 mb-5 rounded-lg overflow-hidden">
+								<ProgressiveImage
+									src={require('../assets/img/appDesign/cart.png')}
+									placeholder={require('../assets/img/appDesign/cart_tn.jpg')}
+								>
+									{(src) => <img className="w-full" src={src} alt="work1" />}
+								</ProgressiveImage>
+							</div>
+							{/* work1 */}
+						</div>
 
-				<div
-					className="my-5 py-8 px-5 md:px-12 w-11/12 md:w-3/4 mx-auto bg-secondaryDark rounder smoothAfShadow"
-					id="work"
-				>
-					asd
-				</div>
-				<div
-					className="my-5 py-8 px-5 md:px-12 w-11/12 md:w-3/4 mx-auto bg-secondaryDark rounder smoothAfShadow"
-					id="work"
-				>
-					asd
+						<div className="w-full p-5 overflow-hidden text-center md:w-1/2 lg:w-1/3 mb-5">
+							{/* work6 */}
+							<div className="shadow m-7 mb-5 rounded-lg overflow-hidden">
+								<ProgressiveImage
+									src={require('../assets/img/appDesign/store.png')}
+									placeholder={require('../assets/img/appDesign/store_tn.jpg')}
+								>
+									{(src) => <img className="w-full" src={src} alt="work3" />}
+								</ProgressiveImage>
+							</div>
+							{/* work6 */}
+						</div>
+					</div>
 				</div>
 			</div>
 		</Fragment>
